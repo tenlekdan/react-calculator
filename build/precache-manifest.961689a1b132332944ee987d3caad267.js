ff608b65550728e41f80d99a72a2e859b7e2537b
tenlekdan <tenlekdan2005@yahoo.co.in>
2020-05-07 17:00:31 +0530

    build

---
+++ build/precache-manifest.961689a1b132332944ee987d3caad267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065972d233069cd59bb8218545a8368e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ec84281a6b13b9b842",
+    "url": "/static/css/main.9b0ecac0.chunk.css"
+  },
+  {
+    "revision": "779b20abc57b16651700",
+    "url": "/static/js/2.d4cc76b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4cc76b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ec84281a6b13b9b842",
+    "url": "/static/js/main.b7c042fc.chunk.js"
+  },
+  {
+    "revision": "761307f7764ba8b0944f",
+    "url": "/static/js/runtime-main.ea8ec4d1.js"
+  }
+]);
